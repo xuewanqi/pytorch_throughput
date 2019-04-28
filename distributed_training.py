6b6f0bb7768d707cb56fdc0db9d766c3f6d2979b
xuewanqi <36396136+xuewanqi@users.noreply.github.com>
2019-04-28 13:46:36 +0800

    tidy codes

--- distributed_training.py
+++ distributed_training.py
@@ -23,18 +23,9 @@ from torch.utils.data import Dataset, DataLoader
 
 from resnet import resnet50
 
-# model_names = sorted(name for name in models.__dict__
-#     if name.islower() and not name.startswith("__")
-#     and callable(models.__dict__[name]))
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
-# parser.add_argument('data', metavar='DIR',
-#                     help='path to dataset')
-# parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
-#                     choices=model_names,
-#                     help='model architecture: ' +
-#                         ' | '.join(model_names) +
-#                         ' (default: resnet18)')
+
 parser.add_argument('-j', '--workers', default=1, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
 parser.add_argument('--epochs', default=1, type=int, metavar='N',
@@ -87,23 +78,6 @@ best_acc1 = 0
 def main():
     args = parser.parse_args()
 
-    # if args.seed is not None:
-    #     random.seed(args.seed)
-    #     torch.manual_seed(args.seed)
-    #     cudnn.deterministic = True
-    #     warnings.warn('You have chosen to seed training. '
-    #                   'This will turn on the CUDNN deterministic setting, '
-    #                   'which can slow down your training considerably! '
-    #                   'You may see unexpected behavior when restarting '
-    #                   'from checkpoints.')
-
-    # if args.gpu is not None:
-    #     warnings.warn('You have chosen a specific GPU. This will completely '
-    #                   'disable data parallelism.')
-
-    # if args.dist_url == "env://" and args.world_size == -1:
-    #     args.world_size = int(os.environ["WORLD_SIZE"])
-
     args.distributed = args.world_size > 1 or args.multiprocessing_distributed #True
 
     ngpus_per_node = torch.cuda.device_count() #8
@@ -134,11 +108,11 @@ def main_worker(gpu, ngpus_per_node, args):
             # For multiprocessing distributed training, rank needs to be the
             # global rank among all the processes
             torch.cuda.set_device(args.gpu)
-            print(1)
+           
             args.rank = args.rank * ngpus_per_node + gpu
         dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
                                 world_size=args.world_size, rank=args.rank)
-        print(2)
+ 
     # create model
     # if args.pretrained:
     #     print("=> using pre-trained model '{}'".format(args.arch))
@@ -148,7 +122,6 @@ def main_worker(gpu, ngpus_per_node, args):
     #     model = models.__dict__[args.arch]()
 
     model=resnet50()
-    print(3)
     if args.distributed:
         # For multiprocessing distributed, DistributedDataParallel constructor
         # should always set the single device scope, otherwise,
@@ -162,7 +135,7 @@ def main_worker(gpu, ngpus_per_node, args):
             args.batch_size = int(args.batch_size / ngpus_per_node)
             #args.workers = int(args.workers / ngpus_per_node)
             model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
-            print(4)
+           
         # else:
         #     model.cuda()
         #     # DistributedDataParallel will divide and allocate batch_size to all
@@ -358,140 +331,6 @@ def train(inputs, steps, model, criterion, optimizer, epoch, args):
         ave_backward_throughput.append(bwd_throughput)
 
     return ave_start, ave_forward_throughput,ave_backward_throughput
-
-
-        # compute output
-        
-
-        # # measure accuracy and record loss
-        # acc1, acc5 = accuracy(output, target, topk=(1, 5))
-        # losses.update(loss.item(), input.size(0))
-        # top1.update(acc1[0], input.size(0))
-        # top5.update(acc5[0], input.size(0))
-
-        # compute gradient and do SGD step
-    
-
-        # # measure elapsed time
-        # batch_time.update(time.time() - end)
-        # end = time.time()
-
-        # if i % args.print_freq == 0:
-        #     progress.print(i)
-
-
-# def validate(val_loader, model, criterion, args):
-#     batch_time = AverageMeter('Time', ':6.3f')
-#     losses = AverageMeter('Loss', ':.4e')
-#     top1 = AverageMeter('Acc@1', ':6.2f')
-#     top5 = AverageMeter('Acc@5', ':6.2f')
-#     progress = ProgressMeter(len(val_loader), batch_time, losses, top1, top5,
-#                              prefix='Test: ')
-
-#     # switch to evaluate mode
-#     model.eval()
-
-#     with torch.no_grad():
-#         end = time.time()
-#         for i, (input, target) in enumerate(val_loader):
-#             if args.gpu is not None:
-#                 input = input.cuda(args.gpu, non_blocking=True)
-#             target = target.cuda(args.gpu, non_blocking=True)
-
-#             # compute output
-#             output = model(input)
-#             loss = criterion(output, target)
-
-#             # measure accuracy and record loss
-#             acc1, acc5 = accuracy(output, target, topk=(1, 5))
-#             losses.update(loss.item(), input.size(0))
-#             top1.update(acc1[0], input.size(0))
-#             top5.update(acc5[0], input.size(0))
-
-#             # measure elapsed time
-#             batch_time.update(time.time() - end)
-#             end = time.time()
-
-#             if i % args.print_freq == 0:
-#                 progress.print(i)
-
-#         # TODO: this should also be done with the ProgressMeter
-#         print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
-#               .format(top1=top1, top5=top5))
-
-#     return top1.avg
-
-
-# def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
-#     torch.save(state, filename)
-#     if is_best:
-#         shutil.copyfile(filename, 'model_best.pth.tar')
-
-
-# class AverageMeter(object):
-#     """Computes and stores the average and current value"""
-#     def __init__(self, name, fmt=':f'):
-#         self.name = name
-#         self.fmt = fmt
-#         self.reset()
-
-#     def reset(self):
-#         self.val = 0
-#         self.avg = 0
-#         self.sum = 0
-#         self.count = 0
-
-#     def update(self, val, n=1):
-#         self.val = val
-#         self.sum += val * n
-#         self.count += n
-#         self.avg = self.sum / self.count
-
-#     def __str__(self):
-#         fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
-#         return fmtstr.format(**self.__dict__)
-
-
-# class ProgressMeter(object):
-#     def __init__(self, num_batches, *meters, prefix=""):
-#         self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
-#         self.meters = meters
-#         self.prefix = prefix
-
-#     def print(self, batch):
-#         entries = [self.prefix + self.batch_fmtstr.format(batch)]
-#         entries += [str(meter) for meter in self.meters]
-#         print('\t'.join(entries))
-
-#     def _get_batch_fmtstr(self, num_batches):
-#         num_digits = len(str(num_batches // 1))
-#         fmt = '{:' + str(num_digits) + 'd}'
-#         return '[' + fmt + '/' + fmt.format(num_batches) + ']'
-
-
-# def adjust_learning_rate(optimizer, epoch, args):
-#     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-#     lr = args.lr * (0.1 ** (epoch // 30))
-#     for param_group in optimizer.param_groups:
-#         param_group['lr'] = lr
-
-
-# def accuracy(output, target, topk=(1,)):
-#     """Computes the accuracy over the k top predictions for the specified values of k"""
-#     with torch.no_grad():
-#         maxk = max(topk)
-#         batch_size = target.size(0)
-
-#         _, pred = output.topk(maxk, 1, True, True)
-#         pred = pred.t()
-#         correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-#         res = []
-#         for k in topk:
-#             correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-#             res.append(correct_k.mul_(100.0 / batch_size))
-#         return res
-
 
 if __name__ == '__main__':
     torch.multiprocessing.set_start_method('spawn')
